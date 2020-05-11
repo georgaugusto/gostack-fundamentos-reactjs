83e7109564bb7fafedeedca83db76ccc8095130c
Georg Augusto Schlegel <georgaugusto@gmail.com>
2020-05-10 23:28:15 -0300

    challenge complete

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { useLocation, Link } from 'react-router-dom';
+
+import { Container } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  size?: 'small' | 'large';
+}
+
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/" className={pathname === '/' ? 'active' : ''}>
+            Listagem
+          </Link>
+          <Link to="/import" className={pathname === '/import' ? 'active' : ''}>
+            Importar
+          </Link>
+        </nav>
+      </header>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/utils/formatValue.ts
@@ -0,0 +1,6 @@
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pr-BR', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  );
+
+export default formatValue;
